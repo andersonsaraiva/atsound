e5832211caa735f1254a0cb2f46586663585d28a
Iorgen Wildrik <w.drik@outlook.com>
2020-07-21 20:04:49 -0300

    feat: add sequelize and models

---
+++ backend/src/database/migrations/20200721225646-create-users.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password_reset_token: {
+        type: Sequelize.STRING,
+        defaultValue: undefined
+      },
+      password_reset_expires: {
+        type: Sequelize.DATE,
+        defaultValue: null
+      },
+      role: {
+        type: Sequelize.STRING,
+        defaultValue: 'user',
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
