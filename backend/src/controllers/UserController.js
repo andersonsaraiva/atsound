76a3fce35347a989442d53068926e3f25f7d7e90
Iorgen Wildrik <w.drik@outlook.com>
2020-07-21 08:44:59 -0300

    add create user route

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,9 +1,25 @@
 const knex = require("../database");
 
 module.exports = {
-  async index(req, res) {
-    const results = await knex("users");
+  async index(req, res, next) {
+    try {
+      const results = await knex("users");
 
-    return res.json(results);
+      return res.json(results);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async create(req, res, next) {
+    try {
+      const { username } = req.body;
+
+      await knex("users").insert({ username });
+
+      return res.status(201).send();
+    } catch (error) {
+      next(error);
+    }
   },
 };
