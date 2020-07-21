9784a6231e5779951b10807eca01f1b33e613413
Iorgen Wildrik <w.drik@outlook.com>
2020-07-21 08:20:05 -0300

    add UserController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,8 @@
 const express = require("express");
-const knex = require("./database");
 const routes = express.Router();
 
-routes.get("/users", (req, res) => {
-  knex("users").then((results) => {
-    return res.json(results);
-  });
-});
+const UserController = require("./controllers/UserController");
+
+routes.get("/users", UserController.index);
 
 module.exports = routes;
