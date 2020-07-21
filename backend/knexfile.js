10b9a8bd7cadbbb035631d1847b67a6c60b73c41
Iorgen Wildrik <w.drik@outlook.com>
2020-07-21 08:06:39 -0300

    add first seed

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -13,6 +13,9 @@ module.exports = {
       tableName: "knex_migrations",
       directory: `${__dirname}/src/database/migrations`,
     },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`,
+    },
   },
 
   // staging: {
