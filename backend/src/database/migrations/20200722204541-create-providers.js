4e052ffb84754f1a55e2d196f90bb7e3c6f404f5
Iorgen Wildrik <w.drik@outlook.com>
2020-07-22 17:51:31 -0300

    feat: add providers migration

---
+++ backend/src/database/migrations/20200722204541-create-providers.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('providers', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('providers');
+  }
+};
+
+// yarn sequelize migration:create --name=create-users
+// yarn sequelize db:migrate
