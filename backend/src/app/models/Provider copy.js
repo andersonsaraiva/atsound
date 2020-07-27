3f322d53d7ae7e32eae76c32486823e06cecd25d
Iorgen Wildrik <w.drik@outlook.com>
2020-07-27 20:22:19 -0300

    feat: add Employee model

---
+++ backend/src/app/models/Provider copy.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Employee extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        phone: DataTypes.STRING,
+        date_of_birth: DataTypes.DATE,
+        gender: DataTypes.STRING,
+        nationality: DataTypes.STRING,
+        marital_status: DataTypes.STRING,
+        schooling: DataTypes.STRING,
+        cpf: DataTypes.STRING,
+        rg: DataTypes.STRING,
+        cnh: DataTypes.STRING,
+        street: DataTypes.STRING,
+        number: DataTypes.STRING,
+        neighborhood: DataTypes.STRING,
+        complement: DataTypes.STRING,
+        city: DataTypes.STRING,
+        state: DataTypes.STRING
+      },
+      { sequelize }
+    );
+  }
+}
+
+module.exports = Employee;
